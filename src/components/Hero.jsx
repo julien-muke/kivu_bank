ea3595c05e24c0f9bcadccacf78beb22c9365614
julien-muke <julien.muke@gmail.com>
2023-08-30 06:15:07 -0700

    🚀 add animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2" data-aos="fade-right"
-     data-aos-offset="300"
+     data-aos-offset="200"
      data-aos-easing="ease-in-sine">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
@@ -17,7 +17,7 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-row justify-between items-center w-full" data-aos="fade-right"
-     data-aos-offset="300"
+     data-aos-offset="200"
      data-aos-easing="ease-in-sine">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             The Next <br className="sm:block hidden" />{" "}
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full" data-aos="fade-right"
-     data-aos-offset="300"
+     data-aos-offset="200"
      data-aos-easing="ease-in-sine">
           Payment Method.
         </h1>
@@ -44,7 +44,7 @@ const Hero = () => {
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" data-aos="fade-left"
-     data-aos-offset="300"
+     data-aos-offset="200"
      data-aos-easing="ease-in-sine" />
 
         {/* gradient start */}
